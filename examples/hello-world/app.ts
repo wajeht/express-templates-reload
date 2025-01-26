906df3b3b46581b03a67dc2cc062bfaa3ed81c75
wajeht <58354193+wajeht@users.noreply.github.com>
2025-01-25 19:02:02 -0600

    feat: example app

---
+++ examples/hello-world/app.ts
@@ -0,0 +1,36 @@
+import ejs from 'ejs';
+import express from 'express';
+import { expressTemplatesReload } from '../../src/express-templates-reload.js';
+
+const app = express();
+
+app.use(express.static('./public'));
+
+app.engine('html', ejs.renderFile);
+
+app.set('view engine', 'html');
+
+app.set('views', './views');
+
+expressTemplatesReload({
+  app,
+  watch: [
+    // Watch a specific file
+    { path: './public/style.css' },
+    { path: './public/script.js' },
+
+    // Watch a directory with specific extensions
+    {
+      path: './views',
+      extensions: ['.html'],
+    },
+  ],
+});
+
+app.get('/', (req: express.Request, res: express.Response) => {
+  return res.render('hello-world.html');
+});
+
+app.listen(80, () => {
+  console.log('App was started on http://localhost');
+});
